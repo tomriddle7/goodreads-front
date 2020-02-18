20b13f9df3440db651fe0b67f7bc4f7106b7a845
tomriddle7 <tomriddle7@naver.com>
2020-02-18 11:43:38 +0900

    Update Header
    https://codesandbox.io/s/mq48rlj0pp

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,77 +1,201 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
 import styled from "styled-components";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Header = styled.header`
-  color: white;
-  position: fixed;
-  top: 0;
-  left: 0;
+const Navigation = styled.header`
   width: 100%;
-  height: 50px;
-  display: flex;
-  align-items: center;
-  background-color: #56CC9D;
+  border-bottom: 1px solid #222;
   z-index: 10;
-`;
-
-const List = styled.ul`
   display: flex;
-`;
-
-const Item = styled.li`
-  width: 20vw;
-  height: 50px;
-  font-size: 18px;
-  text-align: center;
-  color: 
-    ${props => (props.current ? "#ffffff" : "#DDDDDD")};
-  transition: border-bottom 0.5s ease-in-out;
-`;
-
-const SLink = styled(Link)`
-  height: 50px;
-  display: flex;
+  justify-content: space-between;
   align-items: center;
-  justify-content: center;
+  height: 80px;
+  margin-bottom: 60px;
+  background: #f8f8f8;
+
+  .logo a {
+    padding-top: 33px;
+    display: flex;
+    flex-direction: column;
+    clear: both;
+    padding-bottom: 30px;
+    text-decoration: none;
+
+    p {
+      width: 250px;
+      display: block;
+    }
+  }
+  .gray {
+    color: #ccc;
+  }
+  a {
+    color: #222;
+    opacity: 0.55;
+    transition: all 0.6s;
+    color: #222;
+    font-size: 2.4em;
+  }
+  a:hover {
+    opacity: 1;
+  }
+  .fa-bars {
+    display: none;
+    color: #222;
+    font-size: 2rem;
+  }
+  nav {
+    ul {
+      display: flex;
+      justify-content: space-between;
+    }
+    li {
+      margin: 0 15px;
+      justify-content: space-between;
+      font-size: 1.2em;
+    }
+    a {
+      font-size: 1em;
+      text-decoration: none;
+      .active {
+        color: tomato;
+      }
+    }
+    a.active {
+      color: #222;
+    }
+  }
+
+  @media only screen and (max-width: 800px) {
+    padding: 0px;
+    .logo {
+      padding-left: 15px;
+      padding-top: 0px !important;
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    height: auto;
+    min-height: 50px;
+    display: block;
+    position: relative;
+    .logo {
+      width: 100%;
+      display: block;
+      padding-top: 20px;
+      margin: 0px;
+      margin-left: -5px;
+      a {
+        padding: 20px 0px;
+      }
+    }
+    .fa-bars {
+      display: inline-block;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+    ul.collapsed {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      flex-wrap: wrap;
+
+      overflow: hidden;
+      max-height: 0;
+      -moz-transition-duration: 0.4s;
+      -webkit-transition-duration: 0.4s;
+      -o-transition-duration: 0.4s;
+      transition-duration: 0.4s;
+      -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+
+      &.is-expanded {
+        overflow: hidden;
+        max-height: 500px; /* approximate max height */
+        -moz-transition-duration: 0.4s;
+        -webkit-transition-duration: 0.4s;
+        -o-transition-duration: 0.4s;
+        transition-duration: 0.4s;
+        -moz-transition-timing-function: ease-in;
+        -webkit-transition-timing-function: ease-in;
+        -o-transition-timing-function: ease-in;
+        transition-timing-function: ease-in;
+      }
+      li {
+        padding: 15px 10px;
+        margin: 0px 0px;
+        width: 100%;
+      }
+    }
+  }
 `;
 
-export default withRouter(({ location: { pathname } }) => {
-  const Authenticated = window.sessionStorage.getItem("authenticated");
-  console.log(Authenticated === "true");
-  return (
-    <Header>
-      <List>
-        <Item current={pathname === "/"}>
-          <SLink to="/">Home</SLink>
-        </Item>
-        <Item current={pathname === "/search"}>
-          <SLink to="/search">Search</SLink>
-        </Item>
-        <Item current={pathname === "/add"}>
-          <SLink to="/add">Add</SLink>
-        </Item>
-        {Authenticated === "true" ? (
-          <>
-          <Item current={pathname === "/logout"}>
-            <SLink to="/logout">Logout</SLink>
-          </Item>
-          <Item current={pathname === "/shelf"}>
-            <SLink to="/shelf">My Shelf</SLink>
-          </Item>
-        </>
-        ) : (
-          <>
-            <Item current={pathname === "/login"}>
-              <SLink to="/login">Login</SLink>
-            </Item>
-            <Item current={pathname === "/signup"}>
-              <SLink to="/signup">Signup</SLink>
-            </Item>
-          </>
-        )}
-        
-      </List>
-    </Header>
-  );
-});
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+  render() {
+    const { isExpanded } = this.state;
+    const Authenticated = window.sessionStorage.getItem("authenticated");
+    console.log(Authenticated === "true");
+    return (
+      <Navigation>
+      <div className="logo">
+        <Link to="/">
+          <p>GoodReads Clone</p>
+        </Link>
+      </div>
+        <nav className="nav">
+          <FontAwesomeIcon icon={faBars} aria-hidden="true" onClick={e => this.handleToggle(e)} />
+          <ul className={`collapsed ${isExpanded ? "is-expanded" : ""}`}>
+            <NavLink activeClassName="active" to="/">
+              <li>Home</li>
+            </NavLink>
+            <NavLink activeClassName="active" to="/search">
+              <li>Search</li>
+            </NavLink>
+            <NavLink activeClassName="active" to="/add">
+              <li>Add</li>
+            </NavLink>
+            {Authenticated === "true" ? (
+              <>
+                <NavLink activeClassName="active" to="/logout">
+                  <li>Logout</li>
+                </NavLink>
+                <NavLink activeClassName="active" to="/shelf">
+                  <li>Shelf</li>
+                </NavLink>
+            </>
+            ) : (
+              <>
+                <NavLink activeClassName="active" to="/login">
+                  <li>Login</li>
+                </NavLink>
+                <NavLink activeClassName="active" to="/signup">
+                  <li>Signup</li>
+                </NavLink>
+              </>
+            )}
+          </ul>
+        </nav>
+      </Navigation>
+    );
+  }
+}
+
+export default Header;
