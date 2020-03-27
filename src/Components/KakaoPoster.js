4904e986689b38a1a009be9ad3122d1e65c3a555
tomriddle7 <tomriddle7@naver.com>
2020-03-27 14:06:35 +0900

    Update KakaoPoster.js

--- src/Components/KakaoPoster.js
+++ src/Components/KakaoPoster.js
@@ -39,12 +39,48 @@ const Title = styled.span`
 
 const Year = styled.span`
   font-size: 10px;
-  color: rgba(255, 255, 255, 0.5);
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const Actions = styled.div`
+  width: 100%;
+  padding: 10px 5px;
+  margin: auto;
+  text-align: center;
+`;
+
+const ModatlButton = styled.div`
+  cursor: pointer;
+  display: block;
+  padding: 2px 5px;
+  line-height: 20px;
+  font-size: 24px;
+  background: #ffffff;
+  border-radius: 18px;
+  border: 1px solid #cfcece;
 `;
 
 const KakaoPoster = ({ isbn, name, bookImage, author, publisher, pub_year }) => (
-  <div onClick={() => { alert("서적이 등록되었습니다."); booksApi.addBook(isbn); }}>
-    <Container>
+  <Popup trigger={<Container>
+    <ImageContainer>
+      <Image
+        bgUrl={bookImage}
+      />
+    </ImageContainer>
+    <Title>
+      {name.length > 18 ? `${name.substring(0, 18)}...` : name}
+    </Title>
+    <Year>{author}, {pub_year}</Year>
+    <Rating>
+      <span role="img" aria-label="rating">
+        ⭐️
+      </span>{" "}
+      {10}/10
+    </Rating>
+  </Container>} modal>
+    {close => (
+      <div>
+        <Container>
       <ImageContainer>
         <Image
           bgUrl={bookImage}
@@ -61,7 +97,27 @@ const KakaoPoster = ({ isbn, name, bookImage, author, publisher, pub_year }) => 
         {10}/10
       </Rating>
     </Container>
-  </div>
+    <Actions>
+      <ModatlButton
+        onClick={() => {
+          alert("서적이 등록되었습니다.");
+          booksApi.addBook(isbn);
+          close();
+        }}
+      >
+        등록하기
+      </ModatlButton>
+      <ModatlButton
+        onClick={() => {
+          close();
+        }}
+      >
+        취소하기
+      </ModatlButton>
+    </Actions>
+      </div>
+    )}
+  </Popup>
 );
 
 KakaoPoster.propTypes = {
